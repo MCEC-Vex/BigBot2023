f27aec8faab769c31a34a7919d04dfbf9b45a76a
Beatriz Acostaa <beatrizzaacosta29@gmail.com>
2024-04-10 18:15:23 -0700

    Contains auton updates: - catapult shooting - more comments - longer duration of auton movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,171 @@
+BeatrizAcosta
+beatrizacosta
+Invisible
+
+BeatrizAcosta — 03/04/2024 2:06 PM
+Great job fixing the auton!!
+Omm — 03/04/2024 3:07 PM
+Thanks
+It was phoebe and I who fixed it
+Also used you for loop lol
+So it was all of us
+Omm — 03/04/2024 3:37 PM
+How was your hackathon?
+BeatrizAcosta — 03/04/2024 8:23 PM
+Great! I was able to learn how to use Unity to create games and I loved it.
+Omm — 03/04/2024 8:33 PM
+Let's goooo
+I was messing with unity in my python class lol 
+BeatrizAcosta — 03/05/2024 3:39 PM
+You should apply
+Image
+Omm — 03/06/2024 5:02 PM
+Damm
+Send me the link
+Omm — 03/11/2024 6:24 PM
+Hey
+You did a great job today
+BeatrizAcosta — 03/11/2024 6:50 PM
+Thank you! You guys were also great.
+Omm — 03/11/2024 7:31 PM
+Well idk but was something bugging you? Like arre you good? 
+BeatrizAcosta — 03/12/2024 9:53 AM
+No I was fine yesterday just a little nervous before the Girl Scouts came because I didn’t know what to expect.
+BeatrizAcosta — 03/12/2024 10:45 AM
+Also here is the link to the Snapchat internship I forgot to send  https://www.snapacademies.com/sea
+SEA
+Omm — 03/12/2024 11:23 AM
+Oh thanks
+Omm — 03/12/2024 11:23 AM
+Well it went well... let's gooo
+Omm — 03/15/2024 11:06 PM
+each focused on design, marketing, and writing.
+
+click and save one for later.
+
+we recommend starting with dmw_000.
+
+​dmw_000 -- how to leverage the internet​
+
+​dmw_001 -- marketing 101.​
+
+​dmw_002 -- design 101.​
+
+​dmw_003 -- how to write better copy.​
+
+​dmw_004 -- video 101.​
+
+​dmw_005 -- short-form videos 101.​
+
+​dmw_006 -- building your own personal brand.​
+
+more will be dropping on a weekly basis.
+
+let us know what other topics you would like us to cover!
+oh i
+wait
+dmw_000 - https://www.youtube.com/watch?v=jPTIeD7DtKQ&list=PLOHhQHMQ2uzsNNznHWLQX-n7oAtF-EdjS&index=2
+YouTube
+buildspace
+dmw class 0: why design, marketing, and writing matter.
+Image
+https://youtube.com/playlist?list=PLOHhQHMQ2uzsNNznHWLQX-n7oAtF-EdjS&si=jPQJmvtaU931tR6J
+YouTube
+dmw lectures
+introduction to design, marketing, and writing. wtf are these things and why you should care.
+Image
+Image
+Image
+Image
+lol
+Omm — 03/18/2024 3:29 PM
+pros::c::quaternion_s_t qt = imu_sensor.get_quaternion();
+        std::string text = "IMU quaternion: {x: " + std::to_string(qt.x) + ", y: " + std::to_string(qt.y) + ", z: " + std::to_string(qt.z) + ", w: " + std::to_string(qt.w) + "}\n";
+        pros::lcd::set_text(1, text);
+ 
+pros::c::quaternion_s_t qt =imu_sensor.get_quaternion();
+std::string text = "IMU quaternion: {x: " + std::to_string(qt.x) + ", y: " + std::to_string(qt.y) + ", z: " + std::to_string(qt.z) + ", w: " + std::to_string(qt.w) + "}\n";
+pros::lcd::set_text(1, text);
+BeatrizAcosta — 03/25/2024 12:46 PM
+https://sites.google.com/view/mcengineeringclub/home?authuser=0
+MC Engineering Club
+BeatrizAcosta — 03/25/2024 1:17 PM
+Image
+Omm — 03/27/2024 12:09 AM
+Hey, 
+Have a look at this!
+https://omm.needs.rest/MCEC_-_Moorpark_College_Engineering_Club_-_Opera_2024-03-27_00-05-09.mp4
+Ignore the music... I thought it won't capture that
+BeatrizAcosta — 03/27/2024 12:29 AM
+That looks amazing!!!!
+Omm — 03/27/2024 12:50 AM
+Thanks
+Omm — 03/27/2024 2:02 PM
+Hey,
+I was learning the game rules and found this
+Image
+It said this would be the field
+BeatrizAcosta — 03/27/2024 4:26 PM
+That field is for the high school VEX comps
+This is the pic of how Vex U autonomous is set up
+Image
+Omm — 03/27/2024 4:39 PM
+Oh ok
+BeatrizAcosta — 03/28/2024 3:49 PM
+#include "main.h"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+#include <math.h>
+#include <string>
+#include <type_traits>
+Expand
+message.txt
+10 KB
+Omm — 03/28/2024 4:46 PM
+void autonomous()
+{
+	pros::Rotation RotationSensor(12);
+	pros::Imu imu_sensor(19);
+
+	std::shared_ptr<ChassisController> bot = ChassisControllerBuilder()
+Expand
+thing_I_wrote.txt
+3 KB
+Omm — Today at 4:42 PM
+#include "main.h"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+#include <math.h>
+#include <string>
+#include <vector>
+Expand
+auton_new.txt
+12 KB
+BeatrizAcosta — Today at 6:02 PM
+https://github.com/purduesigbots/pros
+GitHub
+GitHub - purduesigbots/pros: Source code for PROS kernel: open sour...
+Source code for PROS kernel: open source C/C++ development for the VEX V5 microcontroller - purduesigbots/pros
+GitHub - purduesigbots/pros: Source code for PROS kernel: open sour...
+Omm — Today at 6:13 PM
+#include "main.h"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+#include <math.h>
+#include <string>
+#include <vector>
+Expand
+message.txt
+13 KB
+﻿
+Omm
+maiommhoon
+|￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
+|      I do tap tap on laptop.  👨‍💻       |
+|   me -    "ommshah.vercel.app"     |
+|＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
+                     \ (•◡•) /
+                       \       /
 #include "main.h"
 #include "pros/misc.h"
 #include "pros/motors.hpp"
@@ -14,8 +182,6 @@ pros::Rotation RotationSensor(12);
 pros::ADIDigitalOut Piston('A');
 pros::Imu imu_sensor(19);
 
-// pros::Motor Catapult();   //add port
-
 /**
  * A callback function for LLEMU's center button.
  *
@@ -28,14 +194,6 @@ pros::Imu imu_sensor(19);
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
-
-
-
-
-
-
-
-//pros::Motor Catapult();   //add port
 
 /**
  * A callback function for LLEMU's center button.
@@ -125,53 +283,65 @@ void competition_initialize() {}
 
 // This set the Catapult's Position
 void setCatapult(){
+	// Set the Rotation Sensor
 	pros::Rotation RotationSensor(12);
-		while (true) // Run Until the end of the autonomous period
+
+	// * Run until the Catault is at the desired position which is Down
+	while (true)
 	{
-		if(RotationSensor.get_angle() < 33998) // If the rotation sensor is less than 33998
+		// * The Rotation Sensor has value with no points so if it's 360.00, it's 36000
+		// If the Rotation Sensor is less than 33998
+		if(RotationSensor.get_angle() < 33998)
 		{
-			Catapult.move_velocity(200); // The catapult goes down
+			// Move the Catapult down
+			Catapult.move_velocity(200);
 		}
-		else // If the rotation sensor is greater than 33998
+		else
 		{
-			Catapult.move_velocity(0); // The catapult stops
+			// Stop the Catapult
+			Catapult.move_velocity(0);
+			// Break the loop
 			break;
 		}
 	}
 }
 
-// Ths function is to get the ball into the Catapult
+// * This function is to get the ball into the Catapult
 void getBall(){
 	setCatapult(); // Set the catapult
 	Intake.move_velocity(-200); // The intake starts
 	Arm.move_absolute(700, 200); // The Arm for intake goes down
 	pros::delay(500); // Wait for the arm to go down
-	for (int i = 0; i >= -1800; i = i - 100) // Slowly (exponentially) move the arm up
+	for (int i = 0; i >= -1500; i = i - 500) // Slowly (exponentially) move the arm up
 	{
 		Intake.move_velocity(-100);
 		Arm.move_absolute(i, 200);
-		pros::delay(200);
-		pros::lcd::set_text(4, "Arm2:" + std::to_string(Arm.get_position()));
+		pros::delay(250);
 	}
 }
 
-// This function is to turn the bot to the left
+// * This function is to turn the bot to the left
 void negativeTurn(int degrees)
-	{
+{
+	// !Reset's the IMU's rotation 
     imu_sensor.tare_rotation();
-		bot->turnAngle(-degrees*1_deg);
 
+	bot->turnAngle(-degrees*1_deg); // Turn the bot to the left
+
+	// * Solve the greater Error - so If the error is -40 degrees, ask the bot to turn -40 degrees
 	if(imu_sensor.get_rotation() <= -degrees)
 	{
-		int value = degrees - abs(imu_sensor.get_rotation());
-		bot->turnAngle(-round(value)*1_deg);
+		int value = degrees - abs(imu_sensor.get_rotation()); // Gets The error
+		bot->turnAngle(-round(value)*1_deg); // Turns it
 	}
-	while(imu_sensor.get_rotation() >= -degrees)
+
+	// * Solve minor error, So we turned the bot -40 degrees in above examples 
+	// * but bot is not accurate so it just turned -30 degrees 
+	// * so until it's not greater than or equal to desired degrees, change -10 more degrees
+	while(imu_sensor.get_rotation() <= -degrees)
 	{
-    	bot->turnAngle(-10_deg);
-		std::string text = "IMU rotation: " + std::to_string(imu_sensor.get_rotation());
-		pros::lcd::set_text(0, text.c_str());
-		if(imu_sensor.get_rotation() >= -(degrees))
+    	bot->turnAngle(-10_deg); // Turn the bot 10 degrees to Left
+		if(imu_sensor.get_rotation() >= -(degrees)) // break the loop when it's greatar than it
 		{
 			break;
 		}
@@ -181,27 +351,66 @@ void negativeTurn(int degrees)
 // This function is to turn the bot to the right
 void positiveTurn(int degrees)
 {
+	// !Reset's the IMU's rotation 
 	imu_sensor.tare_rotation();
 	setCatapult(); // Set the catapult
-	bot->turnAngle(degrees*1_deg);
+	bot->turnAngle(degrees*1_deg); // Turn the bot to the right
 
+	// * Solve the greater Error - so If the error is 40 degrees, ask the bot to turn 40 degrees
 	if(round(imu_sensor.get_rotation()) <= degrees)
 	{
-		int value = abs(imu_sensor.get_rotation()) - degrees;
-		bot->turnAngle(-round(value)*1_deg);
+		int value = abs(imu_sensor.get_rotation()) - degrees; // Gets The error
+		bot->turnAngle(-round(value)*1_deg); // Turns it
 	}
+
+	// * Solve minor error, So we turned the bot 40 degrees in above examples 
+	// * but bot is not accurate so it just turned 30 degrees 
+	// * so until it's not greater than or equal to desired degrees, change 10 more degrees
 	while(round(imu_sensor.get_rotation()) <= degrees)
 	{
-    	bot->turnAngle(10_deg);
-		std::string text = "IMU rotation: " + std::to_string(imu_sensor.get_rotation());
-		pros::lcd::set_text(0, text.c_str());
-		if(imu_sensor.get_rotation() >= degrees)
+    	bot->turnAngle(10_deg); // Turn the bot 10 degrees to Right
+		if(imu_sensor.get_rotation() >= degrees) // break the loop when it's greatar than it
 		{
 			break;
 		}
 	}
 }
 
+void redAuton() {
+
+	// * Part 1 - Set the Catapult, move forward, turn to the left
+	setCatapult();
+	bot->moveDistance(24_in);
+	negativeTurn(70);
+
+	// * Part 2 - Move forward, Get the ball and turn to the left for launching
+	bot->moveDistance(18_in);
+	getBall();
+	negativeTurn(125);
+
+	// * Part 3 - Launch the ball, have a delay and turn to the right to get another ball
+	Catapult.move_velocity(300); // The catapult goes down
+	pros::delay(1000); // Wait for launch
+	positiveTurn(80);
+
+	// * Part 4 - Move forward, get the ball and turn to the left for launching
+	bot->moveDistance(9_in);
+	getBall();
+	negativeTurn(100);
+
+	// * Part 5 - Launch the ball, have a delay and turn to the right to get another ball
+	Catapult.move_velocity(300); // The catapult goes down
+	pros::delay(500); // Wait for launch
+	Catapult.move_velocity(0); // The catapult stops
+
+	// * Part 6 - Move forward, have a delay and move foward for THE SLAPP TO THE BALL
+	positiveTurn(70);
+	pros::delay(100);
+	bot->moveDistance(3.8_ft);
+
+	pros::delay(1000); // Wait for launch
+	pros::lcd::set_text(5, std::to_string('The Program ends'));
+}
 
 void autonomous()
 {
@@ -209,33 +418,12 @@ void autonomous()
 
 	pros::lcd::set_text(1, "THIS IS AUTON!");
 
-	setCatapult();
-	bot->moveDistance(24_in);
-	negativeTurn(80);
+	// ! Call the redAuton function when needed
+	redAuton();
 
-	
-	pros::delay(50); // Wait for the intake to start spinning
-	bot->moveDistance(18_in);
-	pros::delay(50); // Wait for the ball to be in the intake
-	
-	pros::delay(50);
-	getBall();
-	negativeTurn(130);
-
-	Catapult.move_velocity(300); // The catapult goes down
-	pros::delay(1000); // Wait for launch
-	positiveTurn(95);
-
-	bot->moveDistance(9_in);
-	getBall();
-
-	negativeTurn(120);
-
-	Catapult.move_velocity(300); // The catapult goes down
-	pros::delay(1000); // Wait for launch
-	pros::lcd::set_text(5, std::to_string('The Program ends'));
+	// TODO: Code the blueAuton function
+	// blueAuton();
 }
-	// }
 
 /**
  * Runs the operator control code. This function will be started in its own task
